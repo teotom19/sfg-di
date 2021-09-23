640cbc4d5d0d99c17cef4986a018434a68b8b5db
tomadakis <tomadakis@oktopay.eu>
2021-09-23 14:17:43 +0300

    Initial commit

---
+++ src/main/java/com/ttomadakis/tomdi/TomDiApplication.java
@@ -0,0 +1,13 @@
+package com.ttomadakis.tomdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomDiApplication.class, args);
+	}
+
+}
