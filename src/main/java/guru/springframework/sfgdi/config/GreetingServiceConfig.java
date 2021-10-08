8cf9a56e4e36c3d68f1d1a6d79e866692cee213c
tomadakis <tomadakis@oktopay.eu>
2021-10-08 18:25:20 +0300

    --

---
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,45 @@
+package guru.springframework.sfgdi.config;
+
+import guru.springframework.sfgdi.services.*;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Controller;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService(){
+        return new I18nEnglishGreetingService();
+    }
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18NSpanishService i18NSpanishService(){
+        return new I18NSpanishService();
+    }
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
+
+    @Bean
+    ConstructorGreetingService constructorGreetingService() {
+        return new ConstructorGreetingService();
+    }
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService(){
+        return new PropertyInjectedGreetingService();
+    }
+
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService(){
+        return new SetterInjectedGreetingService();
+    }
+}
