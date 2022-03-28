5a7742c1cd0e55ec054626ffe95fada65301b738
tomadakis <tomadakis@oktopay.eu>
2022-03-28 22:48:25 +0300

    --

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -17,13 +17,11 @@ import org.springframework.context.annotation.*;
 public class GreetingServiceConfig {
 
     @Bean
-    FakeDataSource fakeDataSource(@Value("${guru.username}") String username,
-                                  @Value("${guru.password}") String password,
-                                  @Value("${guru.jdbcurl}") String jdbcurl){
+    FakeDataSource fakeDataSource(SfgConfiguration configuration){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUsername(username);
-        fakeDataSource.setPassword(password);
-        fakeDataSource.setJdbcurl(jdbcurl);
+        fakeDataSource.setUsername(configuration.getUsername());
+        fakeDataSource.setPassword(configuration.getPassword());
+        fakeDataSource.setJdbcurl(configuration.getJdbcurl());
         return fakeDataSource;
     }
 
